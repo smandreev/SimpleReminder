8a11e4f66807d53772ff103a1e9c649744723a1f
Sergey.Andreev <sergey.andreev@jetbrains.com>
2015-07-22 19:05:23 +0300

    value changed 55->65 TABSC-14

--- SReminder/src/main/java/com/smandreev/simplereminder/DisplayMessageActivity.java
+++ SReminder/src/main/java/com/smandreev/simplereminder/DisplayMessageActivity.java
@@ -24,7 +24,7 @@ public class DisplayMessageActivity extends Activity {
 
         // Create the text view
         TextView textView = new TextView(this);
-        textView.setTextSize(665);
+        textView.setTextSize(68);
         textView.setText(message);
 
         // Set the text view as the activity layout
